31afb414b28dd5ed97d39bf52fc3cf79c0b2e891
mousav26 <amousavi@me.com>
2019-11-30 19:31:57 -0500

    Image labeller

--- src/fruit_box.py
+++ src/fruit_box.py
@@ -1,22 +1,32 @@
 from PIL import Image
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-image = Image.open("platefruits.jpg")
-image = np.array(image)
+parser = argparse.ArgumentParser(description='Object labelling tool')
+parser.add_argument('--input',
+                    help='Image source')
 
-showCrosshair = False
-fromCenter = False
-r = cv2.selectROI("Image", image, fromCenter, showCrosshair)
+args = parser.parse_args()
+
+image_name = args.input
+image = cv2.imread(image_name)
+
+image = cv2.resize(image, (image.shape[1] // 4, image.shape[0] // 4))
+# image = np.array(image)
+
+show_crosshair = False
+from_center = False
+r = cv2.selectROI("Image", image, from_center, show_crosshair)
 # Crop image
-imCrop = image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+img_crop = image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
 
 # Display cropped image
-print(imCrop.shape, type(imCrop))
+print(img_crop.shape, type(img_crop))
 #cv2.imshow("Image", imCrop)
-plt.imshow(imCrop)
+plt.imshow(img_crop)
 plt.show()
 
-im = Image.fromarray(imCrop)
-im.save("cropped.jpeg")
+im = Image.fromarray(img_crop)
+im.save("cropped.jpg")
