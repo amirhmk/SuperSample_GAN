d9b846bd500ff7de6e5cd57471df1ad730ae6f51
mousav26 <amousavi@me.com>
2019-12-02 21:11:37 -0500

    Fix model shapes

--- src/super_resolute/model.py
+++ src/super_resolute/model.py
@@ -47,19 +47,15 @@ class Discriminator(nn.Module):
             nn.BatchNorm2d(conv_dim * 8),
         )
         self.layer9 = nn.Sequential(
-            nn.Conv2d(
-                conv_dim * 8, conv_dim * 8, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(conv_dim * 8),
-        )
-        self.layer10 = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(
-                conv_dim * 8, conv_dim * 16, kernel_size=1, stride=1, padding=1),
+                conv_dim * 8, conv_dim * 16, kernel_size=1, stride=1, padding=0),
         )
 
-        self.last = nn.Conv2d(conv_dim * 16, 1, kernel_size=1, stride=1)
+        self.last = nn.Conv2d(conv_dim * 16, 1, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
+        print(self.conv_dim)
         out = F.relu(self.layer1(x))
         out = F.relu(self.layer2(out))
         out = F.relu(self.layer3(out))        
@@ -69,17 +65,19 @@ class Discriminator(nn.Module):
         out = F.relu(self.layer7(out))
         out = F.relu(self.layer8(out))
         out = F.relu(self.layer9(out))
-        out = F.relu(self.layer10(out))
-        out = torch.sigmoid(self.last(out).view(self.conv_dim))
+        out = self.last(out)
+        print("oh myyyy", out.shape)
+        out = torch.sigmoid(out.view(x.shape[0]))
         return out
 
 
 class Generator(nn.Module):
     def __init__(self, conv_dim, scale_factor):
         super(Generator, self).__init__()
-        self.upsample_factor = scale_factor
+        self.scale_factor = scale_factor
 
-        self.layer1 = nn.Conv2d(3, conv_dim, kernel_size=9, stride=1, padding=4)
+        self.layer1 = nn.Conv2d(
+            3, conv_dim, kernel_size=9, stride=1, padding=4)
 
         self.residual_layers = nn.Sequential(
             ResidualBlock(conv_dim),
@@ -90,12 +88,13 @@ class Generator(nn.Module):
         )
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(conv_dim, conv_dim, kernel_size=1, stride=1, padding=1),
+            nn.Conv2d(conv_dim, conv_dim, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(conv_dim)
         )
 
         upsample_layers = []
-        for _ in range(scale_factor / 2):
+        for _ in range(self.scale_factor // 2):
+            print("hi", conv_dim)
             upsample_layers.append(UpsampleBLock(conv_dim))
         
         self.upsample_layers = nn.Sequential(
@@ -106,11 +105,14 @@ class Generator(nn.Module):
             conv_dim, 3, kernel_size=9, stride=1, padding=4)
     
     def forward(self, x):
+        print("x", x.shape, x.type(), self.layer1(x).type())
         orig_out = F.relu(self.layer1(x))
         out = self.residual_layers(orig_out)
-        out = self.layer2(out)
+        out = self.layer2(out + orig_out)
+        print("holaaa", out.shape, orig_out.shape)
         out = self.upsample_layers(out)
-        out = self.layer3(out + orig_out)
+        print("anoda one", out.shape, orig_out.shape)
+        out = self.layer3(out)
         return out
 
 
@@ -136,15 +138,17 @@ class ResidualBlock(nn.Module):
 
 class UpsampleBLock(nn.Module):
     def __init__(self, conv_dim):
+        print("hooola", conv_dim)
         super(UpsampleBLock, self).__init__()
         self.layer = nn.Sequential(
-            nn.Conv2d(conv_dim, conv_dim * (2 ** conv_dim),
+            nn.Conv2d(conv_dim, conv_dim * 4,
                                 kernel_size=3, stride=1, padding=1),
             nn.PixelShuffle(2)
         )
 
     def forward(self, x):
-        out = F.prelu(self.layer(x))
+        print("x", x.shape)
+        out = F.relu(self.layer(x))
         return out
 
 
