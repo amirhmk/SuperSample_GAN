a54ba98d0a6e0b23efc4c0883ddd7ceb379fa74a
mousav26 <amousavi@me.com>
2019-12-03 19:18:16 -0500

    Match our own config

--- src/leftthomas_SRGAN/test_image.py
+++ src/leftthomas_SRGAN/test_image.py
@@ -11,26 +11,25 @@ from model import Generator
 parser = argparse.ArgumentParser(description='Test Single Image')
 parser.add_argument('--upscale_factor', default=4, type=int,
                     help='super resolution upscale factor')
-parser.add_argument('--test_mode', default='GPU', type=str,
+parser.add_argument('--test_mode', default='CPU', type=str,
                     choices=['GPU', 'CPU'], help='using GPU or CPU')
 parser.add_argument('--image_name', type=str,
                     help='test low resolution image name')
-parser.add_argument('--model_name', default='netG_epoch_4_100.pth',
+parser.add_argument('--weights', default='weights/netG_epoch_4_54.pth',
                     type=str, help='generator model epoch name')
 opt = parser.parse_args()
 
 UPSCALE_FACTOR = opt.upscale_factor
 TEST_MODE = True if opt.test_mode == 'GPU' else False
 IMAGE_NAME = opt.image_name
-MODEL_NAME = opt.model_name
+weights_path = opt.weights
 
 model = Generator(UPSCALE_FACTOR).eval()
 if TEST_MODE:
     model.cuda()
-    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+    model.load_state_dict(torch.load(weights_path))
 else:
-    model.load_state_dict(torch.load(
-        'epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load(weights_path, map_location=lambda storage, loc: storage))
 
 image = Image.open(IMAGE_NAME)
 image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
